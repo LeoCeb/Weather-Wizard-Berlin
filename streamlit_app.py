f7c5e875d009f6662de2e134b9c2c0b51f8769fa
Leonardo Ceballos <ceballos_leo@hotmail.com>
2023-09-15 13:07:48 +0200

    Added authentication

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,21 @@
 import gspread
 import pandas as pd
 import streamlit as st
-
+from google.oauth2 import service_account
 
 # defining function to get the data from the google spreadsheet
+
+
 def get_data(sheet_name):
+
     # Authenticate and open the Google Sheet
-    gc = gspread.service_account(
-        filename="../service_account.json")
+    credentials_dict = st.secrets['gcp_service_account']
+    credentials = service_account.Credentials.from_service_account_info(credentials_dict, scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive",
+    ])
+
+    gc = gspread.Client(auth=credentials)
     wks = gc.open(sheet_name).sheet1
 
     # Get all the values from the Google Sheet
